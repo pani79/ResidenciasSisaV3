c4881d3a15c9b0046a35c71aca07ff5f51ecf94f
pani79 <emilio.panizza@gmail.com>
2019-08-27 15:55:18 -0300

    dia 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SisaComponent } from './components/sisa/sisa.component';
+import { ResidenciasComponent } from './components/residencias/residencias.component';
+import { HeaderComponent } from './components/interface/header/header.component';
+import { FooterComponent } from './components/interface/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SisaComponent,
+    ResidenciasComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sisa/sisa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sisa',
+  templateUrl: './sisa.component.html',
+  styleUrls: ['./sisa.component.css']
+})
+export class SisaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
